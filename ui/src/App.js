5b0807a66aa78aff0b952b3a047887b05c1d9106
milonite <milonite@hotmail.it>
2020-06-06 10:36:14 +0200

    feat: init cra

---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
